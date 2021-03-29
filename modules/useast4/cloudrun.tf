3d35b86a132d3346e0f7980e9fb687cd13552386
Gautam Mekkat <Blue9@users.noreply.github.com>
2021-03-28 23:18:35 -0400

    Add VPC and Artifact Registry, update DB and Cloud Run

--- modules/useast4/cloudrun.tf
+++ modules/useast4/cloudrun.tf
@@ -4,25 +4,28 @@ resource "google_cloud_run_service" "jimoserver" {
   location = var.region_code
 
   traffic {
-    percent = 100
+    percent         = 100
+    latest_revision = true
   }
+
+  autogenerate_revision_name = true
 
   template {
     spec {
       containers {
-        # Use the image variable defined in main/variables.tf
-        image = var.image
+        # Use the image variable if defined, otherwise default to hello world
+        image = var.image == "" ? "gcr.io/cloudrun/hello" : var.image
 
         # Open port 80
         ports {
-          name           = "APIGateway port"
+          name           = "http1"
           container_port = 80
         }
 
         # Get the connection name dynamically from the DB
         env {
-          name  = "DATABASE_URL"
-          value = "postgresql://postgres@${google_sql_database_instance.master_db.private_ip_address}/postgres?host=/cloudsql/${google_sql_database_instance.master_db.connection_name}"
+          name  = var.image == "" ? null : "DATABASE_URL"
+          value = var.image == "" ? null : "postgresql://${google_sql_user.db_user.name}:${google_sql_user.db_user.password}@${google_sql_database_instance.master_db.private_ip_address}/${google_sql_database.main.name}?host=/cloudsql/${google_sql_database_instance.master_db.connection_name}"
         }
 
         resources {
@@ -35,18 +38,42 @@ resource "google_cloud_run_service" "jimoserver" {
         }
       }
       # No idea what this value should be. Needs testing.
-      container_concurrency = 200
+      container_concurrency = 80
 
       # No idea what this value should be. Needs testing.
-      timeout_seconds = 10
+      timeout_seconds = 30
     }
 
     metadata {
       annotations = {
-        "autoscaling.knative.dev/minScale"      = 1
-        "autoscaling.knative.dev/maxScale"      = 10
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.master_db.connection_name
+        "autoscaling.knative.dev/minScale"        = 1
+        "autoscaling.knative.dev/maxScale"        = 10
+        "run.googleapis.com/cloudsql-instances"   = google_sql_database_instance.master_db.connection_name
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.vpc_connector.name
+        "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
+        "client.knative.dev/user-image"           = var.image == "" ? null : var.image
+
+        # There are auto-added by Cloud Build, might as well just keep them here
+        "run.googleapis.com/client-name"    = "gcloud"
+        "run.googleapis.com/client-version" = "333.0.0"
+        "run.googleapis.com/sandbox"        = "gvisor"
       }
     }
   }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.jimoserver.location
+  project     = google_cloud_run_service.jimoserver.project
+  service     = google_cloud_run_service.jimoserver.name
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
--- modules/useast4/db.tf
+++ modules/useast4/db.tf
@@ -1,6 +1,12 @@
+resource "google_sql_database" "main" {
+  name     = "main"
+  instance = google_sql_database_instance.master_db.name
+}
+
 resource "google_sql_database_instance" "master_db" {
   name             = "master-instance"
-  database_version = "POSTGRES_12"
+  database_version = "POSTGRES_13"
+  depends_on       = [google_service_networking_connection.private_vpc_connection]
   region           = var.region_code
 
   settings {
@@ -8,28 +14,33 @@ resource "google_sql_database_instance" "master_db" {
     # https://cloud.google.com/sql/docs/mysql/instance-settings#settings-2ndgen
     tier = "db-f1-micro"
 
-    # Only allow connections within the VPC
+    # Unfortunately it looks like we have to enable public IP so cloud build can connect
     ip_configuration {
-      ipv4_enabled = false
-      private_network = "projects/${var.project}/global/networks/default"
-      require_ssl = false
+      ipv4_enabled    = true
+      private_network = google_compute_network.vpc.self_link
     }
 
-    # Tuesday at 11am UTC = 5am EST
+    # Tuesday at 9am UTC = 5am ET
     maintenance_window {
-      day = 2
-      hour = 11
+      day          = 2
+      hour         = 9
       update_track = "stable"
     }
 
     # Back up DB to central region and store 10 backups.
     backup_configuration {
-      enabled = true
+      enabled  = true
       location = "us-central1"
       backup_retention_settings {
-        retention_unit = "COUNT"
+        retention_unit   = "COUNT"
         retained_backups = 10
       }
     }
   }
+}
+
+resource "google_sql_user" "db_user" {
+  name     = var.db_username
+  instance = google_sql_database_instance.master_db.name
+  password = var.db_password
 }
