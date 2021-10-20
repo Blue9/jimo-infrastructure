ba59581100f56044f123677b69a45e79eefa9e9d
Gautam Mekkat <Blue9@users.noreply.github.com>
2021-10-19 23:17:38 -0400

    Add artifact repo for background server builds

--- modules/useast4/artifacts.tf
+++ modules/useast4/artifacts.tf
@@ -6,3 +6,12 @@ resource "google_artifact_registry_repository" "artifact_repo" {
   description   = "server builds"
   format        = "DOCKER"
 }
+
+resource "google_artifact_registry_repository" "background_service_repo" {
+  provider = google-beta
+
+  location      = var.region_code
+  repository_id = "jimo-background-service"
+  description   = "background service builds"
+  format        = "DOCKER"
+}
